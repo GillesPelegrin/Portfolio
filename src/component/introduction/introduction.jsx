a2ba4aad10b60c85621fee2f6f760ba8ee9a1c50
Gilles Pelegrin <gilles@wheelhouse.be>
2022-08-10 15:22:12 +0200

    update css from navbar and introduction

--- src/component/introduction/introduction.jsx
+++ src/component/introduction/introduction.jsx
@@ -2,13 +2,13 @@ import working from "../body/working.png";
 
 export default function Introduction() {
   return (
-    <div className="mx-4 flex min-h-[66%] items-center  justify-center md:mx-20 xl:mx-40">
+    <div className="mx-3 flex min-h-[66%] items-center  justify-center md:mx-6 lg:mx-20 2xl:mx-40">
       <div className="order-2  flex flex-col-reverse content-center  justify-center lg:flex-grow  lg:flex-row lg:justify-between">
         <div className="mt-14 text-center lg:text-left">
-          <div className="bg-gradient-to-r from-red-400 to-primary bg-clip-text font-sans text-4xl font-semibold text-transparent lg:text-5xl xl:text-6xl">
+          <div className="bg-gradient-to-r from-red-400 to-primary bg-clip-text font-sans text-3xl font-semibold text-transparent md:text-5xl lg:text-6xl 2xl:text-8xl">
             Full stack developer
           </div>
-          <div className="font-serif text-2xl font-bold text-color-title lg:text-3xl xl:text-4xl">
+          <div className="font-serif text-base font-bold text-color-title md:text-2xl lg:text-3xl 2xl:text-5xl">
             Creating your websites/applications
           </div>
         </div>
@@ -16,7 +16,7 @@ export default function Introduction() {
           <img
             src={working}
             alt="Cartoon of Gilles Pelegrin"
-            className="h-auto w-72 lg:h-80 lg:w-80 xl:h-96 xl:w-96"
+            className="h-auto w-72 lg:h-80 lg:w-80 xl:h-[398px] xl:w-[422px]"
           ></img>
         </div>
       </div>
--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { ReactComponent as IconMenu } from "./logo.svg";
+import { ReactComponent as Logo } from "./logo.svg";
 
 export default function Navbar() {
   const [openMobileNav, setOpenMobileNav] = useState(false);
@@ -8,8 +8,8 @@ export default function Navbar() {
   return (
     <>
       <nav className="mx-4 flex h-1/6  justify-between font-sans font-semibold text-color-title md:mx-10 lg:mx-20">
-        <div className="flex items-center gap-4 xl:gap-6">
-          <IconMenu className=" h-12 w-12 lg:h-16 lg:w-16 xl:h-20 xl:w-20" />
+        <div className="md: flex items-center gap-4 lg:gap-6">
+          <Logo className=" h-12 w-12 md:h-16 md:w-16 xl:h-20 xl:w-20 2xl:h-24 2xl:w-24" />
           <div className=" text-3xl lg:text-4xl xl:text-5xl ">
             Gilles Pelegrin
           </div>
@@ -22,7 +22,7 @@ export default function Navbar() {
           <GiHamburgerMenu size={30} />
         </button>
         <div className=" hidden cursor-pointer md:flex ">
-          <ul className="flex flex-col text-2xl text-color-title md:flex-row  md:space-x-6   lg:space-x-8 lg:text-3xl xl:text-4xl">
+          <ul className=" flex flex-row   gap-4 text-2xl text-color-title lg:text-3xl xl:gap-12 xl:text-4xl">
             <li className="md:flex md:items-center">
               <a className="block hover:text-color-text">Languages</a>
             </li>
@@ -31,7 +31,7 @@ export default function Navbar() {
             </li>
             <li className="md:flex md:items-center">
               {/* This button does not respons the right way, i have the feeling that the border is removed when hovering */}
-              <a className="block rounded-2xl  border-4 border-secundary p-2 text-secundary hover:bg-secundary hover:text-white">
+              <a className="block rounded-2xl  border-4 border-secundary p-1 text-secundary hover:bg-secundary hover:text-white lg:p-2">
                 Contact
               </a>
             </li>
@@ -39,7 +39,7 @@ export default function Navbar() {
         </div>
       </nav>
       {openMobileNav && (
-        <div className="absolute inset-x-0 mx-4 my-4 block cursor-pointer bg-white  font-sans font-semibold shadow-lg md:hidden">
+        <div className="absolute inset-x-0 m-3 block cursor-pointer bg-white  font-sans font-semibold shadow-lg md:hidden">
           <ul className=" mx-4 py-8 text-2xl text-color-title  ">
             <li className="mb-2">
               <a className="hover:text-color-text">Languages</a>
