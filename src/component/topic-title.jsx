f9abe02c0c0475711ee358333853e16312414060
Gilles Pelegrin <gilles.pelegrin@external.engie.com>
2024-01-12 13:40:10 +0100

    new design

--- src/component/topic-title.jsx
+++
@@ -1,7 +0,0 @@
-export default function TopicTitle({ title }) {
-  return (
-    <div className="font-sans text-4xl font-semibold  text-color-title md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-7xl">
-      {title}
-    </div>
-  );
-}
